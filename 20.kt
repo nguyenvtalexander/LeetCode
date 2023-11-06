344ae1d50e84cea020f3a4869b26cf7f67105855
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-06 09:58:35 -0800

    Leetcode 20 Big O

--- 20.kt
+++ 20.kt
@@ -1,3 +1,21 @@
+/*
+Classic stack problem
+iterate through string
+If it's an opening bracket, add it to the stack
+If it's a closing bracket, check if the stack is empty
+if it's not, pop the top of the stack and check if it's not equal to the matching opening bracket
+return false 
+If we fully interate through the string, check if there are any remaining opening brackets in the stack
+Return a check for whether of not the stack is empty
+
+loop is O(N) where N is the size of the input string
+space is O(N) because N can be up to the size of the stack
+
+Final:
+Time - O(N)
+Space - O(N)
+ */
+
 class Solution {
     fun isValid(s: String): Boolean {
         val stack = mutableListOf<Char>()
