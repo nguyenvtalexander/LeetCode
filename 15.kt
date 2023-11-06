1a14e05c0613e4b16e9ccbe81ccc61defd1f9fb8
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-06 09:53:26 -0800

    LeetCode 15 Big O

--- 15.kt
+++ 15.kt
@@ -1,3 +1,25 @@
+/*
+Similar to two-sum
+Initiate results set
+  - use set such that there aren't duplicate results
+Sort nums
+Iterate through nums.lastIndex - 1
+For each num, nums[i +1] until nums.lastIndex 
+Create a left and right pointer
+While left is less than right calculate sum
+If it equals the target, append the list to the set
+If it's less, increase the left pointer, else decrement right pointer
+return results as a list
+
+sort is O(nlog(n))
+loops are O(N^2)
+space O(1) || O(N) - sorting might take extra memory in some libraries
+
+Final: 
+Time - O(N^2)
+Space - O(1) for Kotlin
+*/
+
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> {
         val results = HashSet<List<Int>>()
