7984105a74d75174ab66b42437f1717de5058e42
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-06 09:44:10 -0800

    Leetcode 49

---
+++ 49.kt
@@ -0,0 +1,11 @@
+class Solution {
+  fun groupAnagrams(strs: Array<String>): List<List<String>> {
+      val map = HashMap<String, MutableList<String>>()
+      for (str in strs) {
+          val key = str.toCharArray().sorted().toString()
+          map[key] = map.getOrDefault(key, mutableListOf<String>())
+          map[key]?.add(str)
+      }
+      return map.values.toList()
+  }
+}
