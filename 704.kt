8f923257bc8d536a53f8b36578d0b20130940f30
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-06 10:45:46 -0800

    704 Big O

--- 704.kt
+++ 704.kt
@@ -1,17 +1,19 @@
+// Classic Binary search, log(N), log(N) if recursive, O(1) if iterative
+
 class Solution {
-  fun search(nums: IntArray, target: Int): Int {
-      var l = 0
-      var r = nums.lastIndex
-      while (l <= r) {
-          val mid = (l + r) / 2
-          if (nums[mid] == target) {
-              return mid
-          } else if (nums[mid] > target) {
-              r --
-          } else {
-              l ++
-          }
-      }
-      return -1
-  }
+    fun search(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.lastIndex
+        while (l < r) {
+            val mid = (l + r) / 2
+            if (nums[mid] == target) {
+                return mid
+            } else if (nums[mid] > target) {
+                r --
+            } else {
+                l ++
+            }
+        }
+        return -1
+    }
 }
