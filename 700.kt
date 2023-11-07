8080fcce30e389ffc95b9471083cc3c0c6a57ecf
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-07 15:30:00 -0800

    Leetcode 700

---
+++ 700.kt
@@ -0,0 +1,27 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+  fun searchBST(root: TreeNode?, target: Int): TreeNode? {
+      var cur = root
+
+      while (cur != null) {
+          val curVal = cur?.`val`
+          if (curVal == target) {
+              return cur
+          } else if (curVal != null && curVal > target) {
+              cur = cur.left
+          } else {
+              cur = cur.right
+          }
+      }
+      return null
+  }
+}
