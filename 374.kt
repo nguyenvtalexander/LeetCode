4551306eec1e0235d7548640aae57ecfa3a2a420
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-07 15:30:33 -0800

    Leetcode 374

---
+++ 374.kt
@@ -0,0 +1,28 @@
+/** 
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+ class Solution:GuessGame() {
+  override fun guessNumber(n:Int):Int {
+      var l = 0
+      var r = n
+
+      while (l <= r) {
+          val mid = l + (r - l) / 2
+          val g = guess(mid)
+          if (g == 0) {
+              return mid
+          } else if (g > 0) {
+              l = mid + 1
+          } else {
+              r = mid - 1
+          }
+      }
+      return -1
+  }
+}
