058a226ec80e49a16fa817ce39f95e0dacebd0dc
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-08 13:01:20 -0800

    Leetcode 206 recursive

--- 206.kt
+++ 206.kt
@@ -20,3 +20,29 @@ class Solution {
       return prev
   }
 }
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ * 
+ * Recursive solution
+ */
+
+class Solution {
+  fun reverseList(head: ListNode?): ListNode? {
+      if (head == null || head.next == null) {
+          return head
+      }
+
+      val newHead = reverseList(head.next)
+      head.next?.next = head
+      head.next = null
+
+      return newHead
+  }
+}
