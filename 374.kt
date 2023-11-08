a34951f3277d305b75db8076ebd4ff2c9612558e
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-08 14:13:18 -0800

    Update 374 solution

--- 374.kt
+++ 374.kt
@@ -26,3 +26,30 @@
       return -1
   }
 }
+
+/** 
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+ class Solution:GuessGame() {
+    override fun guessNumber(n:Int):Int {
+        var l = 0
+        var r = n
+
+        while (l <= r) { 
+            val mid = l + (r - l) / 2
+            when (guess(mid)) {
+                0 -> return mid
+                -1 -> r = mid - 1
+                1 -> l = mid + 1
+            }
+        }
+
+        return -1
+    }
+}
