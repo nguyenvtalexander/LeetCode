a117185642b1843576c29ff97d4a77af13011b8c
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-08 14:01:01 -0800

    Updated 704 mistake

--- 704.kt
+++ 704.kt
@@ -4,14 +4,15 @@ class Solution {
     fun search(nums: IntArray, target: Int): Int {
         var l = 0
         var r = nums.lastIndex
-        while (l < r) {
+
+        while (l <= r) {
             val mid = (l + r) / 2
             if (nums[mid] == target) {
                 return mid
-            } else if (nums[mid] > target) {
-                r --
+            } else if (nums[mid] < target) {
+                l = mid + 1
             } else {
-                l ++
+                r = mid - 1
             }
         }
         return -1
