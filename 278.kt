ea562da8489174a2bb08c4a916eba3386d7077f3
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-10 13:40:32 -0800

    Updated Leetcode 278 & Leetcode 704

--- 278.kt
+++ 278.kt
@@ -27,3 +27,31 @@ class Solution: VersionControl() {
       }
       return lbv
 }
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} 
+
+    first check if n/2 is bad 
+    if it is bad that means that either this or one before it is the first bad version
+
+    if it isn't bad, that means the first bad version has to be after this
+*/
+
+class Solution: VersionControl() {
+    override fun firstBadVersion(n: Int) : Int {
+        var l = 0
+        var r = n
+        var bv = 0
+        while (l <= r) {
+            val mid = l + (r - l) / 2
+            when (isBadVersion(mid)) {
+                false -> l = mid + 1
+                else -> {
+                    bv = mid
+                    r = mid - 1
+                }
+            }
+        }
+        return bv
+	}
+}
--- 704.kt
+++ 704.kt
@@ -18,3 +18,19 @@ class Solution {
         return -1
     }
 }
+
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.lastIndex
+        while (l <= r) {
+            val mid = l + (r - l) / 2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] > target -> r = mid - 1
+                else -> l = mid + 1
+            }
+        }
+        return -1
+    }
+}
