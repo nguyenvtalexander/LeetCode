e474a4d139bcf26de6ff9b0237a9d65e29d7b89c
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-10 14:19:28 -0800

    Updated Leetcode 102 solution

--- 102.kt
+++ 102.kt
@@ -31,3 +31,34 @@ class Solution {
       return res
   }
 }
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+  fun levelOrder(root: TreeNode?): List<List<Int>> {
+    val res = mutableListOf<List<Int>>()
+    if (root == null) return res
+    val que = ArrayDeque<TreeNode>()
+    que.add(root)
+
+    while (!que.isEmpty()) {
+        val level = mutableListOf<Int>()
+        for (i in que.indices) {
+            val node = que.removeFirst()
+            level.add(node.`val`)
+            if (node.left != null) que.add(node.left)
+            if (node.right != null) que.add(node.right)
+        }
+        res.add(level)
+    }
+    return res
+  }
+}
