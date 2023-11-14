a8746ec8c911e2e1df53c3b02f9aebe34e6e1597
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-14 08:12:33 -0800

    Updated leetcode 509

--- 509.kt
+++ 509.kt
@@ -9,3 +9,22 @@ class Solution {
       return fib(n - 1) + fib(n - 2)
   }
 }
+
+/**
+ * memo implmentation
+ */
+
+class Solution {
+  fun fib(n: Int): Int {
+      val memo = IntArray(n + 1) { -1 }
+      return helper(n, memo)
+  }
+
+  fun helper(n: Int, memo: IntArray): Int {
+      if (n == 0) return 0
+      if (n == 1) return 1
+      if (memo[n] != -1) return memo[n]
+      memo[n] = fib(n - 1) + fib(n - 2)
+      return memo[n]
+  }
+}
