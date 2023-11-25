d6defa6dbea072f431c11bee23c57a0fa5d69aeb
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-25 12:01:56 -0800

    Updated leetcode 3'

--- 3.kt
+++ 3.kt
@@ -16,3 +16,21 @@ class Solution {
       return maxLength
   }
 }
+
+class Solution {
+  fun lengthOfLongestSubstring(s: String): Int {
+      val hs = HashSet<Char>()
+      var i = 0
+      var j = 0
+      var maxLength = 0
+      while (j < s.length) {
+          if (hs.contains(s[j])) {
+              hs.remove(s[i++])
+          } else {
+              hs.add(s[j++])
+              maxLength = max(hs.size, maxLength)
+          }
+      }
+      return maxLength
+  }
+}
