b5debf1bcf2451d93264b69376b86a23a2d05039
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-28 14:19:02 -0800

    Updated leetcode 78

--- 78.kt
+++ 78.kt
@@ -37,3 +37,20 @@ class Solution {
       return res
   }
 }
+
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        var res = mutableListOf<List<Int>>()
+        
+        fun dfs(start: Int, sub: MutableList<Int>) {
+            res.add(ArrayList(sub))
+            for (n in start..nums.lastIndex) {
+                sub.add(nums[n])
+                dfs(n + 1, sub)
+                sub.removeAt(sub.size - 1)
+            }
+        }
+        dfs(0, mutableListOf())
+        return res
+    }
+}
