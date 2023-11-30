2434aa7608225d76db2c92c4a013bd616a0c59f4
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-11-29 22:59:12 -0800

    Updated leetcode 160

--- 160.kt
+++ 160.kt
@@ -27,3 +27,52 @@
       return null
   }
 }
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+ class Solution {
+    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+        var pointerA = headA
+        var pointerB = headB
+
+        var lenA = 0
+        var lenB = 0
+
+        var tempA = headA
+        while (tempA != null) {
+            lenA++
+            tempA = tempA.next
+        }
+
+        var tempB = headB
+        while (tempB != null) {
+            lenB++
+            tempB = tempB.next
+        }
+
+        while (lenA > lenB) {
+            pointerA = pointerA?.next
+            lenA--
+        }
+
+        while (lenB > lenA) {
+            pointerB = pointerB?.next
+            lenB--
+        }
+
+        while (pointerA != pointerB) {
+            pointerA = pointerA?.next
+            pointerB = pointerB?.next
+        }
+
+        return pointerA
+    }
+}
